4882b866848ac26df1d5bfb0bdf2608a0170525c
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2025-02-07 20:09:19 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import react from "@vitejs/plugin-react";
+import { keycloakify } from "keycloakify/vite-plugin";
+import path from "path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    keycloakify({
+      accountThemeImplementation: "none",
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
