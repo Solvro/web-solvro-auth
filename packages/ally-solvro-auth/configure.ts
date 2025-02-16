92377fe5d3969b6b51e2a27b4eed04895dc93726
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2025-02-16 19:20:22 +0100

    fix: stub ally config and prompt for ids

--- packages/ally-solvro-auth/configure.ts
+++ packages/ally-solvro-auth/configure.ts
@@ -11,22 +11,36 @@ import { stubsRoot } from "./stubs/main.js";
 export async function configure(command: Configure) {
   const codemods = await command.createCodemods();
 
+  command.logger.info("Konfiguracja @solvro/auth");
+  command.logger.info(
+    "Żeby dostać CLIENT_ID i CLIENT_SECRET, zapytaj na #main i zpinguj @Bartosz Gotowski",
+  );
+
+  const clientId = await command.prompt.ask("Jaki masz CLIENT_ID? ", {
+    hint: "web-planer",
+  });
+  const clientSecret = await command.prompt.ask("Jaki masz CLIENT_SECRET? ", {
+    hint: "a17c54tH8AmWC0yq7FSQbNpPp8wELqeN",
+  });
+
   await codemods.makeUsingStub(
     stubsRoot,
     "controllers/auth_controller.stub",
     {},
   );
-  // env variables: APP_DOMAIN, SOLVRO_AUTH_CLIENT_ID
-  // Add environment variables
+  await codemods.makeUsingStub(stubsRoot, "config/ally.stub", {});
+
   await codemods.defineEnvVariables({
     APP_DOMAIN: "http://localhost:3333",
-    SOLVRO_AUTH_CLIENT_ID: "MY_CLIENT_ID",
+    SOLVRO_AUTH_CLIENT_ID: clientId,
+    SOLVRO_AUTH_CLIENT_SECRET: clientSecret,
   });
 
   await codemods.defineEnvValidations({
     variables: {
       APP_DOMAIN: `Env.schema.string()`,
       SOLVRO_AUTH_CLIENT_ID: "Env.schema.string()",
+      SOLVRO_AUTH_CLIENT_SECRET: "Env.schema.string.optional()",
     },
     leadingComment: "Variables for @solvro/auth",
   });
